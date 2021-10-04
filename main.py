3b2da994dc571678b19f032a4c1f698c0157da2e
andrewabsa <farshadabsalan6@gmail.com>
2021-10-04 15:01:04 +1100

    Started making game paddle

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
 #Main Game loop
 while True:
     wn.update()
